d975a62b51a3f8c501f5c18020a7f3bf4fd14118
Adriandmen <adrianmensing1@gmail.com>
2019-03-23 23:30:00 +0100

    add easier board notation

--- local search/main.py
+++ local search/main.py
@@ -1,19 +1,28 @@
 from lib.LocalSearch import LocalSearch
 from lib.board import Board
 
+Q = True
+_ = False
 
 if __name__ == '__main__':
-    # x = Board.from_board([
-    #     [True, False, False, False],
-    #     [False, False, False, True],
-    #     [False, True, False, False],
-    #     [False, True, False, False]
-    # ])
-    l = LocalSearch(8)
-    s = l.search(n=10, retries=50)
+    x = Board.from_board([
+        [Q, _, _, _],
+        [_, Q, _, _],
+        [Q, _, _, _],
+        [_, Q, _, _]
+    ]) # type: Board
 
-    print("{} optimum with cost {} is:".format("Global" if s.cost() == 0 else "Local", s.cost()))
-    print()
-    print(s)
-    print()
+    for neighbour in x.neighbourhood():
+        print(neighbour)
+        print()
+
+    print("{} neighbours found.".format(len(x.neighbourhood())))
+
+    # l = LocalSearch(8)
+    # s = l.search(n=10, retries=50)
+    #
+    # print("{} optimum with cost {} is:".format("Global" if s.cost() == 0 else "Local", s.cost()))
+    # print()
+    # print(s)
+    # print()
     # print(l.search_step(x))
